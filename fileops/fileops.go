dcfd2d9e1f840fa894ba621c0922654eee1c0301
spinatose <spinatose@gmail.com>
2023-03-19 21:26:31 -0500

    added some todo items that i may convert into WI tickets on repo

--- fileops/fileops.go
+++ fileops/fileops.go
@@ -1,5 +1,11 @@
 package fileops
 
 func ValidMachineFolder(folder string) bool {
+	// TODO: check to see if is a valid directory
+	// TODO: check to see if accessible
+	// TODO: check to see if writeable
+	// TODO: check to see if files in folder are moveable
+	// TODO: throw meaningful errors
+
 	return true
 }
--- main.go
+++ main.go
@@ -7,6 +7,14 @@ import (
 	"spinatose.com/mediaxer/fileops" 
 )
 
+// TODO: eventually add command line args to filter certain files
+// TODO: allow running from config file instead of command line- or allow override 
+// TODO: provide flag type sets for filters like "all photos", "all videos", "all media", "all text files", etc...
+// TODO: allow for argument for source and target directories (will have to check both for valid folders)
+// TODO: possibly allow to upload to cloud
+// TODO: possibly create file streaming server to accept incoming "files" stream over network and save to target
+// TODO: create GUI interface
+
 func main() {
 	fmt.Println("      Welcome to mediAxer - file organizer!")
 	fmt.Println("¡Bienvenido a mediAxer - organizador de archivos!")
