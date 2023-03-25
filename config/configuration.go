de3d0afb25abfb9085fbeddfb5b57552c107d963
Scot Pfuntner <spinatose@gmail.com>
2023-03-24 19:50:45 -0500

    changed targetfolder to destinationfolder in config

--- config/configuration.go
+++ config/configuration.go
@@ -2,7 +2,7 @@ package config
 
 type Config struct {
 	MoveSourceFiles 	bool `json:"moveSourceFiles"`
-	TargetFolder 		string `json:"targetFolder"`
+	DestinationFolder 	string `json:"destinationFolder"`
 	SourceFolder		string `json:"sourceFolder"`
 	ResultFolderPattern string `json:"resultFolderPattern"`
 }
